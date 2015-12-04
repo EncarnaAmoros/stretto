1d1459f052a494bb7926657af754e213f473cb92
EncarnaAmoros <encaramorosb@gmail.com>
2015-12-04 09:22:11 +0100

    Archivo de configuración de karma

---
+++ stretto-web/karma.conf.js
@@ -0,0 +1,97 @@
+// Karma configuration
+// Generated on Thu Dec 03 2015 12:16:10 GMT+0100 (Hora estándar romance)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+		
+		// list of files / patterns to load in the browser
+    files: [
+			JASMINE, 
+			JASMINE_ADAPTER,
+			
+			
+			
+			
+			{pattern: 'public/test/strettoService-test.js', watched: false, included: true, served: true}
+			
+			
+			/*'public/lib/angular.js',
+			'public/js/angular.min.js',
+			'node_modules/angular-mocks/angular-mocks.js',
+			'node_modules/angular-mocks/*.js',
+			'app.js',
+			'public/js/app.js',
+			'public/js/strettoService.js',
+			'node_modules/requirejs/require.js',
+			'public/test/strettoService-test.js'*/
+    ],
+
+		preprocessors: {
+    	'public/test/*.js': [ 'browserify' ] //Mention path as per your test js folder
+ 		},
+		
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['requirejs', 'jasmine', 'browserify'],
+		
+		singleRun: true,
+		
+		plugins : [
+        'karma-chrome-launcher',
+        'karma-jasmine',
+        'karma-browserify',
+				'karma-requirejs'
+ 		],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultanous
+    concurrency: Infinity
+  })
+}
