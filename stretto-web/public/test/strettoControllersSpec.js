776f0339a75fd39cba9cc129da5cbd1d05d055fc
EncarnaAmoros <encaramorosb@gmail.com>
2015-12-06 08:27:30 +0100

    Test probando ArticulosCtrl2 (es prueba más simple)
    Bien y con comentarios

--- stretto-web/public/test/strettoControllersSpec.js
+++ stretto-web/public/test/strettoControllersSpec.js
@@ -1,103 +1,39 @@
 'use strict';
 
-/* jasmine specs for controllers go here */
-describe('test de ArticulosCtrl', function() {
+/* jasmine specs para controladores */
 
+describe('specs de los controladores de strettoControllers', function() {
+
+	//Inyectamos el módulo de las rutas
   beforeEach(module('strettoApp'));
+	
+	//Inyectamos el módulo del servicio utilizado por el controlador
   beforeEach(module('strettoService'));
-  describe('PhoneListCtrl', function(){
+	
+	/* TEST DEL CONTROLADOR ARTICULOSCTRL */
+  describe('ArticulosCtrl', function(){
     var scope, ctrl, $httpBackend;
 
+		//Para cada test: creación del controlador inyectando mocks necesarios (scope, etc)
     beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+			//Necesario para devolver lo que queramos que devuelva el servicio mockeado
       $httpBackend = _$httpBackend_;
       $httpBackend.expectGET('http://localhost:3000/stretto/articulos?page=1').
-          respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
-
+          respond({_links: {}, data:[{name: 'Nexus S'}, {name: 'Motorola DROID'}]});
+			
+			//Mock de scope
       scope = $rootScope.$new();
+			//Creamos el controlador a probar asignando scope
       ctrl = $controller('ArticulosCtrl2', {$scope: scope});
     }));
-    it('should create "phones" model with 2 phones fetched from xhr', function() {
-     // expect(scope.articulos).toEqual([]);
+		
+    it('debe crear el modelo de artículos con 2 items', function() {
+     	//Al principio está vacío
+			expect(scope.articulos).toEqual(undefined);
+			//Al realizarse la petición $http obtendremos los datos
       $httpBackend.flush();
-
+			//Deben haber estos dos datos en el scope determinado
       expect(scope.articulos).toEqual([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
     });
   });
 });
-
-
-
-
-/*describe('controller: MainCtrl', function() {
-  var ctrl, foo, $scope;
-
-  beforeEach(module('strettoService'));
-	beforeEach(module('strettoControllers'));
-
-  beforeEach(inject(function($rootScope, $controller) {
-    articulosService = {
-      getArticulos: function() {}
-    };
-    spyOn(articulosService, 'getArticulos').and.returnValue("Foo"); // <----------- HERE
-
-    $scope = $rootScope.$new();
-
-    ctrl = $controller('ArticulosCtrl2', {$scope: $scope , articulosService: articulosService });
-  }));
-
-  it('Should call foo fn', function() {
-    expect($scope.articulos).toBe('Foo');
-  });
-
-});*/
-
-
-
-
-/*describe('Controlador: ArticulosController', function() {
-  var scope, articuloService, httpBackend;
-  
-	//NEWWW-Módulo donde se encuentra el controler
-  beforeEach(module('strettoControllers'));
-	
-	//Creamos el módulo con el mock del servicio vacío
-  beforeEach(function() {
-		var mockStrettoService = {};
-		module('strettoService', function($provide) {
-			$provide.value('articuloService', mockStrettoService);
-  	});
-    
-	//Las peticiones con $http se fijarán en estos datos sin llamar a la API (mock)
-	inject(function($q) {
-  	mockStrettoService.data = [{nombre: 'Angular'},{nombre: 'Ember'}];
-      
-		//Mockeamos el servicio con sus llamadas
-		mockStrettoService.getArticulos = function(page) {
-			var defer = $q.defer();    
-			defer.resolve([{nombre: 'Angular'},{nombre: 'Ember'}]);        
-			return defer.promise;
-		};
-		
-   });
-		
-  });
-	
-	//Inyectamos el mock del servicio que ya no está vacio
-  beforeEach(inject(function($controller, $rootScope, _articuloService_, $httpBackend) {
-    scope = $rootScope.$new();
-    articuloService = _articuloService_;
-		httpBackend = $httpBackend;
-    $controller('ArticulosCtrl2', {$scope: scope, articuloService: articuloService });
-    scope.$digest();
-		console.log("11111");
-		console.log(articuloService);
-		console.log(scope);
-  }));
-  
-  it('debe contener todos los artículos definidos al principio', function() {
-		console.log("1");
-		httpBackend.when('GET', 'http://localhost:3000/stretto/articulos?page=1').passThrough();
-    expect(scope.articulos).toEqual([{nombre: 'Angular'},{nombre: 'Ember'}]);
-  });
-  
-});*/
